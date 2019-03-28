8518af737848cbe9a597a92d6f865794e15f6432
Isaac Priestley <progressions@gmail.com>
2019-03-28 15:34:08 -0400

    Put back the logger

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Greedy.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [],
+      extra_applications: [:logger],
       mod: {Greedy.Application, []}
     ]
   end
