c73b073a180c1bc8bb16c832b4b81bb1d690128b
Isaac Priestley <progressions@gmail.com>
2019-03-29 17:22:17 -0400

    Add ex_doc to make it easier to read the documentation.

--- mix.exs
+++ mix.exs
@@ -8,6 +8,13 @@ defmodule Greedy.MixProject do
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
+    ] ++ docs()
+  end
+
+  def docs do
+    [
+      name: "Greedy",
+      source_url: "https://github.com/progressions/greedy",
     ]
   end
 
@@ -23,7 +30,8 @@ defmodule Greedy.MixProject do
   defp deps do
     [
       {:kafka_ex, "~> 0.9.0"},
-      {:avro_ex, "~> 0.1.0-beta.0"}
+      {:avro_ex, "~> 0.1.0-beta.0"},
+      {:ex_doc, "~> 0.19", [only: :dev, runtime: false]},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
